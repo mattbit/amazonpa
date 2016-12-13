6c7aa922e8c610b227b74cf4e904c92dae88fcb4
Stefano Brilli <stefanobrilli@gmail.com>
2016-12-13 08:42:32 +0100

    Update tests to new changes

--- response_test.go
+++ response_test.go
@@ -251,30 +251,36 @@ func TestParseItemLookupResponse(t *testing.T) {
 	assertEqualStr(t, response.Items.Request.ItemLookupRequest.VariationPage, "All", "Bad VariationPage")
 
 	//Items/Item
-	assertEqualStr(t, response.Items.Items[0].ASIN, "B003TGG2EA", "Bad ASIN")
-	assertEqualStr(t, response.Items.Items[0].DetailPageURL, "https://www.amazon.it/Grohe-32843000-Cosmopolitan-Miscelatore-Monocomando/dp/B003TGG2EA%3FSubscriptionId%3DAKIAIZL74FSKHXDX66WQ%26tag%3Dgoldbot-21%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB003TGG2EA", "Bad DetailPageURL")
-	assertEqualInt(t, response.Items.Items[0].SalesRank, 214, "Bad SalesRank")
+	assertEqualStr(t, response.Items.Item.ASIN, "B003TGG2EA", "Bad ASIN")
+	assertEqualStr(t, response.Items.Item.DetailPageURL, "https://www.amazon.it/Grohe-32843000-Cosmopolitan-Miscelatore-Monocomando/dp/B003TGG2EA%3FSubscriptionId%3DAKIAIZL74FSKHXDX66WQ%26tag%3Dgoldbot-21%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB003TGG2EA", "Bad DetailPageURL")
+	assertEqualInt(t, response.Items.Item.SalesRank, 214, "Bad SalesRank")
 
-	assertEqualStr(t, response.Items.Items[0].SmallImage.URL, "http://ecx.images-amazon.com/images/I/3143fcCtWnL._SL75_.jpg", "Bad SmallImage/URL")
-	assertEqualInt(t, int(response.Items.Items[0].SmallImage.Height), 75, "Bad SmallImage/Height")
-	assertEqualInt(t, int(response.Items.Items[0].SmallImage.Width), 52, "Bad SmallImage/Width")
+	assertEqualStr(t, response.Items.Item.SmallImage.URL, "http://ecx.images-amazon.com/images/I/3143fcCtWnL._SL75_.jpg", "Bad SmallImage/URL")
+	assertEqualInt(t, int(response.Items.Item.SmallImage.Height), 75, "Bad SmallImage/Height")
+	assertEqualInt(t, int(response.Items.Item.SmallImage.Width), 52, "Bad SmallImage/Width")
 
-	assertEqualStr(t, response.Items.Items[0].MediumImage.URL, "http://ecx.images-amazon.com/images/I/3143fcCtWnL._SL160_.jpg", "Bad SmallImage/URL")
-	assertEqualInt(t, int(response.Items.Items[0].MediumImage.Height), 160, "Bad SmallImage/Height")
-	assertEqualInt(t, int(response.Items.Items[0].MediumImage.Width), 110, "Bad SmallImage/Width")
+	assertEqualStr(t, response.Items.Item.MediumImage.URL, "http://ecx.images-amazon.com/images/I/3143fcCtWnL._SL160_.jpg", "Bad SmallImage/URL")
+	assertEqualInt(t, int(response.Items.Item.MediumImage.Height), 160, "Bad SmallImage/Height")
+	assertEqualInt(t, int(response.Items.Item.MediumImage.Width), 110, "Bad SmallImage/Width")
 
-	assertEqualStr(t, response.Items.Items[0].LargeImage.URL, "http://ecx.images-amazon.com/images/I/3143fcCtWnL.jpg", "Bad SmallImage/URL")
-	assertEqualInt(t, int(response.Items.Items[0].LargeImage.Height), 500, "Bad SmallImage/Height")
-	assertEqualInt(t, int(response.Items.Items[0].LargeImage.Width), 344, "Bad SmallImage/Width")
+	assertEqualStr(t, response.Items.Item.LargeImage.URL, "http://ecx.images-amazon.com/images/I/3143fcCtWnL.jpg", "Bad SmallImage/URL")
+	assertEqualInt(t, int(response.Items.Item.LargeImage.Height), 500, "Bad SmallImage/Height")
+	assertEqualInt(t, int(response.Items.Item.LargeImage.Width), 344, "Bad SmallImage/Width")
 
 	// Items/Item/ItemAttributes
-	assertEqualStr(t, response.Items.Items[0].ItemAttributes.Binding, "Tools & Home Improvement", "Bad Binding")
-	assertEqualStr(t, response.Items.Items[0].ItemAttributes.Brand, "Grohe", "Bad Binding")
-	assertEqualStr(t, response.Items.Items[0].ItemAttributes.Color, "Cromo", "Bad Color")
-	assertEqualStr(t, response.Items.Items[0].ItemAttributes.Creator, "", "Bad Creator")
-	assertEqualStr(t, response.Items.Items[0].ItemAttributes.EAN, "4005176874840", "Bad EAN")
+	assertEqualStr(t, response.Items.Item.ItemAttributes.Binding, "Tools & Home Improvement", "Bad Binding")
+	assertEqualStr(t, response.Items.Item.ItemAttributes.Brand, "Grohe", "Bad Binding")
+	assertEqualStr(t, response.Items.Item.ItemAttributes.Color, "Cromo", "Bad Color")
+	assertEqualStr(t, response.Items.Item.ItemAttributes.Creator, "", "Bad Creator")
+	assertEqualStr(t, response.Items.Item.ItemAttributes.EAN, "4005176874840", "Bad EAN")
 
-	assertEqualInt(t, int(response.Items.Items[0].ItemAttributes.ListPrice.Amount), 18500, "Bad Price/Amount")
-	assertEqualStr(t, response.Items.Items[0].ItemAttributes.ListPrice.CurrencyCode, "EUR", "Bad Price/Currency")
-	assertEqualStr(t, response.Items.Items[0].ItemAttributes.ListPrice.FormattedPrice, "EUR 185,00", "Bad Price/Formatted Price")
+	assertEqualInt(t, int(response.Items.Item.ItemAttributes.ListPrice.Amount), 18500, "Bad Price/Amount")
+	assertEqualStr(t, response.Items.Item.ItemAttributes.ListPrice.CurrencyCode, "EUR", "Bad Price/Currency")
+	assertEqualStr(t, response.Items.Item.ItemAttributes.ListPrice.FormattedPrice, "EUR 185,00", "Bad Price/Formatted Price")
+
+	assertEqualStr(t, response.Items.Item.BrowseNodes.BrowseNode[0].BrowseNodeID, "3120323031", "Bad BrowseNode/BrowseNodeID")
+	assertEqualStr(t, response.Items.Item.BrowseNodes.BrowseNode[0].Name, "Rubinetti per lavelli da cucina", "Bad BrowseNode/Name")
+
+	assertEqualStr(t, response.Items.Item.BrowseNodes.BrowseNode[0].Ancestors.BrowseNode[0].BrowseNodeID, "3119756031", "Bad Ancestors/BrowseNodeID")
+	assertEqualStr(t, response.Items.Item.BrowseNodes.BrowseNode[0].Ancestors.BrowseNode[0].Name, "Rubinetti da cucina", "Bad Ancestors/Name")
 }
